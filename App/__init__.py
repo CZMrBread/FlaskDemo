a48ab63a01b472e1a57da9a791dd743c40fa195f
Dominik Smola <dsmola9@gmail.com>
2025-03-20 20:00:32 +0100

    example

--- App/__init__.py
+++ App/__init__.py
@@ -1,15 +1,22 @@
 from dotenv import load_dotenv
 from flask import Flask, session, redirect, flash, url_for
-from .config import DevelopConfig
+from App.config import DevelopConfig
 from App.extensions import db
-from .app import my_app
+from App.app import my_app
+from Models.User import User
+from faker import Faker
 load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(DevelopConfig)
-
     db.init_app(app)
+
+
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+
     app.register_blueprint(my_app)
     return app
--- App/app.py
+++ App/app.py
@@ -1,7 +1,35 @@
-from flask import render_template, Blueprint
+import flask
+from flask import render_template, Blueprint,redirect, flash, url_for
+from App import db
+from faker import Faker
+from Models.User import User
 my_app = Blueprint("my_app", __name__, template_folder='../templates')
 
 
 @my_app.route('/')
-def index():
-    return render_template("index.html")
+def show_users():
+    return render_template("show.html")
+
+@my_app.route('/create-user')
+def create_user():
+    return render_template("show.html")
+
+
+@my_app.route('/modify-user')
+def create_user():
+    return render_template("show.html")
+
+
+
+@my_app.route('/generate')
+def generate_fake_users(n=20):
+    fake = Faker('cs_CZ')
+    for _ in range(n):
+        user = User(
+            username=fake.unique.user_name(),
+            email=fake.unique.email()
+        )
+        db.session.add(user)
+    db.session.commit()
+    flash("Users generated", "success")
+    return redirect(url_for('my_app.show_users'))
